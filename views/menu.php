429cb0b12bb4de5a7c414779ba085d864ff4f096
nellcap <nell.capelle@protonmail.com>
2026-03-04 15:21:58 +0100

    menu

--- views/menu.php
+++ views/menu.php
@@ -1,1 +1,29 @@
-Hello !
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Suivi satellite des zones de reforestation de l’association Treecolor</title>
+    <link rel="stylesheet" href="assets/styles.css">
+</head>
+<body>
+    <div id="entete">
+        <h1>Suivi satellite des zones de reforestation de l’association Treecolor</h1>
+    </div>
+
+    <div id="menu">
+        <div id= "panama">
+            <img src="data/panama/carte.jpg" alt="Carte du Panama">
+        </div>
+        <ul>
+            <li><a href="/burkina"> Carte du Burkina Faso </a></li>
+            <li><a href="/panama"> Carte du Panama </a></li>
+        </ul>
+    </div>
+
+    <div id="footer">
+        <p>AUGUSTIN Esteban, CAPELLE Nell, GOUGET Louise</p>
+    </div>
+</body>
+</html>
+
