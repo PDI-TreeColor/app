34d05d2069ae91334d32faace9b3727127a4ee1e
nellcap <nell.capelle@protonmail.com>
2026-03-18 15:03:29 +0100

    suppression projet

--- index.php
+++ index.php
@@ -1,3 +1,73 @@
+<?php
+
+    function ajouterZone(){
+        require_once '/var/www/html/vendor/autoload.php';
+
+        $conn = pg_connect("host=db dbname=mydb user=treecolor password=treecolor");
+        $nom = $_POST['projet'];
+
+        $kmlContent = file_get_contents($_FILES['kml']['tmp_name']);
+
+        // Conversion KML -> GeoJSON
+        $geometry = geoPHP::load($kmlContent, 'kml');
+
+        $geojsonData = $geometry->out('json');   
+        $geojsonArray = json_decode($geojsonData, true);
+        $geometryJson = json_encode($geojsonArray);
+
+        $dossier = '/var/www/html/data/' . $nom . '/';
+        if(!is_dir($dossier)){
+            mkdir($dossier, 0777, true);
+        }
+
+        file_put_contents($dossier . $nom . '.geojson', json_encode([
+            'type' => 'FeatureCollection',
+            'features' => [[
+                'type' => 'Feature',
+                'geometry' => $geojsonArray,
+                'properties' => ['nom' => $nom]
+            ]]
+        ]));
+
+        $nomImage = $nom . '/' . basename($_FILES['image']['name']);
+        $destination = $dossier . basename($_FILES['image']['name']);
+        move_uploaded_file($_FILES['image']['tmp_name'], $destination);
+
+        $query = "
+            INSERT INTO zones (nom, geom, image)
+            VALUES (
+                $1,
+                ST_SetSRID(ST_GeomFromGeoJSON($2),4326),
+                $3
+            )
+        ";
+
+        $result = pg_query_params($conn, $query, [$nom, $geometryJson, $nomImage]);
+
+    }
+
+    function supprimerZone(){
+        $conn = pg_connect("host=db dbname=mydb user=treecolor password=treecolor");
+        $id = $_POST['supprimer_zone'];
+        $query = "DELETE FROM zones WHERE id = $1";
+        pg_query_params($conn, $query, [$id]);
+    }
+
+
+    if(isset($_POST['ajouter_zone'])){
+        ajouterZone();
+        header('Location: ' . $_SERVER['PHP_SELF']);
+        exit();
+    }
+
+    if(isset($_POST['supprimer_zone'])){
+        supprimerZone();
+        header('Location: ' . $_SERVER['PHP_SELF']);
+        exit();
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,24 +77,7 @@
     <title>Accueil TreeColor</title>
     <link href="style.css" rel="stylesheet">
     <style>
-        #PLUS {
-            position: absolute;
-            left: 45%;
-            bottom: 10%;
-            font-size: 20px;
-        }
 
-        
-        #formulaire{
-            position: absolute;
-            left: 45%;
-            bottom: 10%;
-            background:white;
-            padding:20px;
-            border-radius:10px;
-            box-shadow:0 0 10px rgba(0,0,0,0.3);
-            display:none;
-        }
 
 
     </style>
@@ -32,78 +85,31 @@
 <body>
     <h1>Bienvenue sur l'application de suivie des projets TreeColor</h1>
 
-    <?php
-
-        function ajouterZone(){
-            $conn = pg_connect("host=db dbname=mydb user=treecolor password=treecolor");
-
-            $nom = $_POST['projet'];
-
-            $file = $_FILES['geojson']['tmp_name'];
-            $geojson = file_get_contents($file);
-            $data = json_decode($geojson, true);
-
-            $geometry = json_encode($data['features'][0]['geometry']);
-
-            $nomImage = $nom . '/' . basename($_FILES['image']['name']);
-            $dossier = '/var/www/html/data/' . $nom . '/';
-
-            // Crée le dossier si il n'existe pas
-            if(!is_dir($dossier)){
-                mkdir($dossier, 0777, true);
-            }
-
-            $destination = $dossier . basename($_FILES['image']['name']);
-            $moveResult = move_uploaded_file($_FILES['image']['tmp_name'], $destination);
-            
-            $query = "
-                INSERT INTO zones (nom, geom, image)
-                VALUES (
-                    $1,
-                    ST_SetSRID(ST_GeomFromGeoJSON($2),4326),
-                    $3
-                )
-            ";
-
-            $result = pg_query_params($conn, $query, [$nom, $geometry, $nomImage]);
-        }
-
-        if(isset($_POST['ajouter_zone'])){
-            ajouterZone();
-        }
-
-    ?>
-
     <div class="projets-container">
         <?php
-            $dossiers = scandir("./data");
-            foreach($dossiers as $pays){
-                if($pays == '.' || $pays == '..') continue;
+            $conn = pg_connect("host=db dbname=mydb user=treecolor password=treecolor");
+            
+            $result = pg_query($conn, "SELECT id, nom, image FROM zones");
                 
-                $dossier = './data/' . $pays;
-                if(!is_dir($dossier)) continue;
-
-                $image = '';
-                $fichiers = scandir($dossier);
-                foreach($fichiers as $fichier){
-                    if(strpos($fichier, '.png') || strpos($fichier, '.jpg') || strpos($fichier, '.jpeg')){
-                        $image = $fichier;
-                    }
-                }
-
-                $nomImage = 'data/' . $pays . '/' . $image;
-            ?>
+            while($zone = pg_fetch_assoc($result)){
+        ?>
                 <div class="projet">
-                    <h3> Projet <?php echo $pays; ?> </h3>
-                    <img src="<?php echo $nomImage; ?>" class="card-img-top" alt="..." style="width: 300px;">
+                    <h3>Projet <?php echo $zone['nom']; ?></h3>
+                    <img src="data/<?php echo $zone['image']; ?>" class="card-img-top" alt="..." style="width: 300px;">
                     <div class="card-body">
                         <form action="visualisation.php" method="GET">
-                            <input type="hidden" name="projet" value="<?php echo $pays; ?>">
+                            <input type="hidden" name="projet" value="<?php echo $zone['nom']; ?>">
                             <button class="carte-bouton">Voir le projet</button>
+                        </form>
+
+                        <form method="POST">
+                            <button class="bouton-suppr" name="supprimer_zone" value="<?php echo $zone['id']; ?>">Supprimer le projet</button>
                         </form>
                     </div>
                 </div>
-            <?php } ?>
+        <?php
+            }     
+        ?>
     </div>
 
     <div id="PLUS">
@@ -116,8 +122,8 @@
             <label for="projet">Nom du projet :</label><br>
             <input type="text" id="projet" name="projet" required><br><br>
 
-            <label for="file">Fichier GEOJSON :</label><br>
-            <input type="file" id="file" name="geojson" accept=".geojson,.json" required><br><br>
+            <label for="kml">Fichier KML :</label><br>
+            <input type="file" id="kml" name="kml" accept=".kml" required><br><br>
 
             <label for="image">Image de la carte :</label><br>
             <input type="file" id="image" name="image" accept=".png,.jpg,.jpeg" required><br><br>
