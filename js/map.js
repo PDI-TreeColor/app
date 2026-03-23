df7ac1f6bc247b2e80573f3c0077a547c06314a4
EstebanAugustin <esteban.agstn@gmail.com>
2026-03-23 11:55:54 +0100

    images au bon endroit

--- js/map.js
+++ js/map.js
@@ -17,16 +17,6 @@ const osmLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // Ajout d'un gestionnaire de couche
 const layerControl = L.control.layers({'OpenStreetMaps': osmLayer}, {}, {collapsed: false}).addTo(map);
 
-// Ajout de l'outil de mesure (distances et surfaces)
-const measureControl = L.control.measure({
-    position: 'topright',
-    primaryLengthUnit: 'meters', 
-    secondaryLengthUnit: 'kilometers',
-    primaryAreaUnit: 'sqmeters', 
-    secondaryAreaUnit: 'hectares'
-});
-measureControl.addTo(map);
-
 // Afficher la zone du projet
 fetch(`read_db.php?projet=${idProjet}`)
     .then(response => response.json())
@@ -98,3 +88,9 @@ calendar.addEventListener("change", (event) => {
     var date = getSelectedDate();
     updateCopernicusLayers(date);
 });
+
+
+
+
+
+
