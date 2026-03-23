5b75c0686a29f1e02190e63f0de5c65b84df90d3
nellcap <nell.capelle@protonmail.com>
2026-03-23 10:53:24 +0100

    init.sql

--- visualisation.php
+++ visualisation.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test de l'API Copernicus</title>
+    <title>Projet TreeColor</title>
     <!-- Leaflet -->
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
         integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
@@ -16,7 +16,7 @@
         <select name="year" id="year-select"></select>
         <select name="month" id="month-select"></select>
     </div>
-    
+
     <div id="accueil">
         <a href="index.php"><button class="bouton_accueil">Retour à la page d'accueil</button></a>
     </div>
