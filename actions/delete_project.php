b89faf3268fd5ea5b97d386f2b50858a272fba9d
EstebanAugustin <esteban.agstn@gmail.com>
2026-03-25 14:49:19 +0100

    fix bug suppression projet non vide

--- actions/delete_project.php
+++ actions/delete_project.php
@@ -6,17 +6,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['supprimer_zone'])) {
     $conn = getDbConnection();
     $id = $_POST['supprimer_zone'];
 
-    // Nettoyage de l'image stockée dans le dossier /data/{id}/
-    $dossier = __DIR__ . '/../data/' . $id . '/';
-    if (is_dir($dossier)) {
-        $files = glob($dossier . '*');
-        foreach ($files as $file) {
-            if (is_file($file)) {
-                unlink($file);
-            }
+    // Fonction helper pour tout supprimer dans un dossier
+    function supprimerDossierRecursif($dir) {
+        if (!file_exists($dir)) return true;
+        if (!is_dir($dir)) return unlink($dir);
+        foreach (scandir($dir) as $item) {
+            if ($item == '.' || $item == '..') continue;
+            if (!supprimerDossierRecursif($dir . DIRECTORY_SEPARATOR . $item)) return false;
         }
-        rmdir($dossier);
+        return rmdir($dir);
     }
+
+    $dossier = __DIR__ . '/../data/' . $id . '/';
+    supprimerDossierRecursif($dossier);
 
     $query = "DELETE FROM zones WHERE id = $1";
     pg_query_params($conn, $query, [$id]);
